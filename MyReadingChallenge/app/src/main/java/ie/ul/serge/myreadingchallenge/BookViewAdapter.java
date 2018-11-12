22dcc4f217e26bb80f3832152c347efea0c5ed14
SergejsUL <39090869+SergejsUL@users.noreply.github.com>
2018-11-12 19:21:14 +0000

    add arraylist to host snapshots / add connection and query to adaptor constructor

--- MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/BookViewAdapter.java
+++ MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/BookViewAdapter.java
@@ -1,14 +1,48 @@
 package ie.ul.serge.myreadingchallenge;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookViewAdapter extends RecyclerView.Adapter<BookViewAdapter.BookViewHolder>{
 
+    private List<DocumentSnapshot> mBooksSnapshots = new ArrayList<>();
+    public BookViewAdapter(){
+        CollectionReference booklistRef = FirebaseFirestore.getInstance()
+        .collection(Constants.BOOK_COLLECTION);
+        booklistRef.orderBy(Constants.CREATED_KEY, Query.Direction.DESCENDING).limit(50)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(QuerySnapshot documentSnapshots,
+                                        FirebaseFirestoreException e) {
+                        if(e!=null){
+                            Log.w(Constants.TAG,"Listening failed");
+                            return;
+                        }
+                        mBooksSnapshots = documentSnapshots.getDocuments();
+                        notifyDataSetChanged();
+                    }
+
+                });
+
+
+    }
     @NonNull
     @Override
     public BookViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
---
+++ MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/Constants.java
@@ -0,0 +1,8 @@
+package ie.ul.serge.myreadingchallenge;
+
+public class Constants {
+    public static final String BOOK_COLLECTION = "books";
+    public static final String CREATED_KEY="created";
+    public static final String TAG="BK";
+
+}
