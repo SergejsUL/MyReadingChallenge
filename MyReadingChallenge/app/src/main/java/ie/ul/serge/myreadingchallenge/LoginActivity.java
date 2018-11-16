5701ef459b9fbcb828bff55ed1dee996cb6fcaa9
SergejsUL <39090869+SergejsUL@users.noreply.github.com>
2018-11-16 23:01:24 +0000

    login givens added to login activity

--- MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/LoginActivity.java
+++ MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/LoginActivity.java
@@ -2,6 +2,7 @@ package ie.ul.serge.myreadingchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void handleSignIn(View view){
+
+    }
+    public void handleSignUp(View view){
+
     }
 }
