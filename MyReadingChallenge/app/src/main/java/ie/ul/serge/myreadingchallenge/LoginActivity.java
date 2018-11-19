c2b075e4bef0fa47e79bd8b37722b46dae2a5147
SergejsUL <39090869+SergejsUL@users.noreply.github.com>
2018-11-19 15:12:12 +0000

    SKIP LOGIN IF ALREADY SIGNED IN

--- MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/LoginActivity.java
+++ MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/LoginActivity.java
@@ -24,7 +24,15 @@ public class LoginActivity extends AppCompatActivity {
     mEmailEditText = findViewById(R.id.email_edittext);
     mPasswordEditText=findViewById(R.id.password_edittext);
     mAuth=FirebaseAuth.getInstance();
-    }
+
+    // CHECK IF USER IS ALREADY LOGGED IN AND SKIP LOGIN.
+        if (mAuth !=null){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+
+    }//END OF ON CREATE
+
 
     public void handleSignIn(View view){
 
