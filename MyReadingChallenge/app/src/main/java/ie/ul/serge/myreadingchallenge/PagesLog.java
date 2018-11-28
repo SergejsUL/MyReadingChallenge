36a44890e1f013207d76427a578b11c7b2e708b9
SergejsUL <39090869+SergejsUL@users.noreply.github.com>
2018-11-28 15:30:28 +0000

    added PagesLog class

---
+++ MyReadingChallenge/app/src/main/java/ie/ul/serge/myreadingchallenge/PagesLog.java
@@ -0,0 +1,24 @@
+package ie.ul.serge.myreadingchallenge;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class PagesLog {
+
+    private FirebaseFirestore db;
+
+    PagesLog(){
+
+
+    }
+
+    public void logPages(String query, long numberPages)
+    {
+        HashMap<String , Object> pageLog= new HashMap<>();
+
+
+
+
+    }
+}
